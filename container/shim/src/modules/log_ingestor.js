165f4526f8541d04cb67941f3984501700814fc1
Diego Rodriguez Baquero <diego@protocol.ai>
2022-06-01 17:36:24 -0500

    Swap interval with timeout, remove logs per retrieval

--- container/shim/src/modules/log_ingestor.js
+++ container/shim/src/modules/log_ingestor.js
@@ -26,78 +26,10 @@ if (fs.existsSync('/var/log/nginx/node-access.log')) {
   debug('Reading nginx log file')
   fh = await openFileHandle()
 
-  setInterval(async () => {
-    const read = await fh.readFile()
-
-    if (read.length > 0) {
-      hasRead = true
-      const lines = read.toString().trim().split('\n')
-
-      for (const line of lines) {
-        const vars = line.split('&&').reduce((varsAgg, currentValue) => {
-          const [name, ...value] = currentValue.split('=')
-          const jointValue = value.join('=')
-
-          let parsedValue
-          switch (name) {
-            case 'args': {
-              parsedValue = jointValue.split('&').reduce((argsAgg, current) => {
-                const [name, ...value] = current.split('=')
-                return Object.assign(argsAgg, { [name]: value.join('=') })
-              }, {})
-              break
-            }
-            case 'lt':
-            case 'rid':
-            case 'addr': {
-              parsedValue = jointValue
-              break
-            }
-            case 'ucs': {
-              parsedValue = jointValue === 'HIT'
-              break
-            }
-            default: {
-              const numberValue = Number.parseFloat(jointValue)
-              parsedValue = Number.isNaN(numberValue) ? jointValue : numberValue
-            }
-          }
-          return Object.assign(varsAgg, { [NGINX_LOG_KEYS_MAP[name] || name]: parsedValue })
-        }, {})
-
-        if (vars.request?.startsWith('/cid/') && vars.status === 200) {
-          const { clientAddress, numBytesSent, request, requestId, localTime, requestDuration, args, range, cacheHit, referrer, userAgent } = vars
-          const cid = request.replace('/cid/', '')
-          const { clientId } = args
-          debug(`Client ${clientId} at ${clientAddress} from ${referrer} requested ${cid} range: ${range} size: ${Math.floor(numBytesSent / 1024)} KB HIT:${cacheHit} duration: ${requestDuration}ms RID: ${requestId}`)
-
-          pending.push({
-            cacheHit,
-            cid,
-            clientAddress,
-            clientId,
-            localTime,
-            numBytesSent,
-            range,
-            requestDuration,
-            requestId,
-            userAgent
-          })
-        }
-      }
-    } else {
-      if (hasRead) {
-        await fh.truncate()
-        await fh.close()
-        hasRead = false
-        fh = await openFileHandle()
-      }
-    }
-  }, 10_000)
+  parseLogs()
 
   setInterval(async () => {
     if (pending.length > 0) {
-      debug(`Submitting pending ${pending.length} retrievals to wallet ${FIL_WALLET_ADDRESS}`)
       const body = {
         nodeId,
         filAddress: FIL_WALLET_ADDRESS,
@@ -112,12 +44,86 @@ if (fs.existsSync('/var/log/nginx/node-access.log')) {
             'Content-Type': 'application/json'
           }
         })
+        debug(`Submitted pending ${pending.length} retrievals to wallet ${FIL_WALLET_ADDRESS}`)
         pending = []
       } catch (err) {
         debug(err)
       }
     }
   }, 60_000)
+}
+
+async function parseLogs () {
+  const read = await fh.readFile()
+
+  if (read.length > 0) {
+    hasRead = true
+    const lines = read.toString().trim().split('\n')
+
+    let valid = 0
+    for (const line of lines) {
+      const vars = line.split('&&').reduce((varsAgg, currentValue) => {
+        const [name, ...value] = currentValue.split('=')
+        const jointValue = value.join('=')
+
+        let parsedValue
+        switch (name) {
+          case 'args': {
+            parsedValue = jointValue.split('&').reduce((argsAgg, current) => {
+              const [name, ...value] = current.split('=')
+              return Object.assign(argsAgg, { [name]: value.join('=') })
+            }, {})
+            break
+          }
+          case 'lt':
+          case 'rid':
+          case 'addr': {
+            parsedValue = jointValue
+            break
+          }
+          case 'ucs': {
+            parsedValue = jointValue === 'HIT'
+            break
+          }
+          default: {
+            const numberValue = Number.parseFloat(jointValue)
+            parsedValue = Number.isNaN(numberValue) ? jointValue : numberValue
+          }
+        }
+        return Object.assign(varsAgg, { [NGINX_LOG_KEYS_MAP[name] || name]: parsedValue })
+      }, {})
+
+      if (vars.request?.startsWith('/cid/') && vars.status === 200) {
+        const { clientAddress, numBytesSent, request, requestId, localTime, requestDuration, args, range, cacheHit, referrer, userAgent } = vars
+        const cid = request.replace('/cid/', '')
+        const { clientId } = args
+
+        pending.push({
+          cacheHit,
+          cid,
+          clientAddress,
+          clientId,
+          localTime,
+          numBytesSent,
+          range,
+          referrer,
+          requestDuration,
+          requestId,
+          userAgent
+        })
+        valid++
+      }
+    }
+    debug(`Parsed ${valid} valid retrievals`)
+  } else {
+    if (hasRead) {
+      await fh.truncate()
+      await fh.close()
+      hasRead = false
+      fh = await openFileHandle()
+    }
+  }
+  setTimeout(parseLogs, 10_000)
 }
 
 async function openFileHandle () {
