b04bad437a291a3ffb6a91d8691a78bc2dca3b0f
Diego Rodriguez Baquero <diego@protocol.ai>
2022-07-26 15:25:06 -0500

    Update org repo to run

--- update.sh
+++ update.sh
@@ -35,7 +35,7 @@ if [[ $out != *"up to date"* ]]; then
 
   sudo docker stop --time 60 saturn-node || true
   sudo docker rm -f saturn-node || true
-  sudo docker run --name saturn-node -it -d --restart=unless-stopped -v $HOME/shared:/usr/src/app/shared -e FIL_WALLET_ADDRESS=$FIL_WALLET_ADDRESS -e NODE_OPERATOR_EMAIL=$NODE_OPERATOR_EMAIL --network host ghcr.io/filecoin-project/saturn-node:$SATURN_NETWORK
+  sudo docker run --name saturn-node -it -d --restart=unless-stopped -v $HOME/shared:/usr/src/app/shared -e FIL_WALLET_ADDRESS=$FIL_WALLET_ADDRESS -e NODE_OPERATOR_EMAIL=$NODE_OPERATOR_EMAIL --network host ghcr.io/filecoin-saturn/l1-node:$SATURN_NETWORK
   sudo docker image prune -f
 
   echo "Updated to latest version successfully!"
