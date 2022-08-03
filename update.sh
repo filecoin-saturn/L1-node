0f84b869a0a8c9a04c533d160b8b9a131b651d0d
Diego Rodriguez Baquero <diego@protocol.ai>
2022-08-03 18:50:58 -0500

    Introduce SATURN_HOME to allow customizing shared folder place

--- update.sh
+++ update.sh
@@ -3,6 +3,7 @@
 set -ex
 
 : "${SATURN_NETWORK:=test}"
+: "${SATURN_HOME:=$HOME}"
 
 if pidof -o %PPID -x "update.sh" > /dev/null; then
   exit
@@ -10,7 +11,7 @@ fi
 
 echo -n $(date -u) "Checking for update.sh script updates... "
 
-target=$HOME/update.sh
+target=$SATURN_HOME/update.sh
 if wget -O "$target.tmp" -T 10 -t 3 "https://raw.githubusercontent.com/filecoin-saturn/L1-node/main/update.sh" && [[ -s "$target.tmp" ]] && [ $(stat -c %s "$target.tmp") -ne $(stat -c %s "$target") ]
 then
   mv -f "$target.tmp" "$target"
@@ -35,7 +36,7 @@ if [[ $out != *"up to date"* ]]; then
 
   sudo docker stop --time 120 saturn-node || true
   sudo docker rm -f saturn-node || true
-  sudo docker run --name saturn-node -it -d --restart=unless-stopped -v $HOME/shared:/usr/src/app/shared -e FIL_WALLET_ADDRESS=$FIL_WALLET_ADDRESS -e NODE_OPERATOR_EMAIL=$NODE_OPERATOR_EMAIL --network host ghcr.io/filecoin-saturn/l1-node:$SATURN_NETWORK
+  sudo docker run --name saturn-node -it -d --restart=unless-stopped -v $SATURN_HOME/shared:/usr/src/app/shared -e FIL_WALLET_ADDRESS=$FIL_WALLET_ADDRESS -e NODE_OPERATOR_EMAIL=$NODE_OPERATOR_EMAIL --network host ghcr.io/filecoin-saturn/l1-node:$SATURN_NETWORK
   sudo docker image prune -f
 
   echo "Updated to latest version successfully!"
