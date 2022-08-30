f83a8217c0447aa1d8f9cfac79843ae9e62142f3
Julian Gruber <julian@juliangruber.com>
2022-08-30 17:30:05 +0200

    Add L1<>L2 interop. Closes #37 (#40)
    * add basic GET /register/:l2id
    
    * FIXME
    
    * flow works if we disable forking
    
    * clean up
    
    * lint
    
    * fix demo
    
    * block cid
    
    * fix write after end
    
    * add cluster logging
    
    * enable cluster again
    
    * shorten heartbeat interval for testing
    
    * add Cache-Control header
    
    * remove node:cluster
    
    * fix type error
    
    * run multiple node processes
    
    * refactor
    
    * allow testing cid again
    
    * Revert "run multiple node processes"
    
    This reverts commit bffdce2f44778382c17119fda358be4a0d2b630d.
    
    * Revert "remove node:cluster"
    
    This reverts commit e9dfbbd4635668a6e03f216646ec9111a564471d.
    
    * clean up
    
    * only one fork for now
    
    * websocket implementation with flaws
    
    * fix consistent hashing
    
    * fix timeout error handling
    
    * enable cluster again
    
    * reduce to one worker again
    
    * Revert "websocket implementation with flaws"
    
    This reverts commit 034e0f838120dae91622033226ea1a5ee88e89d2.
    
    * heartbeat is just an empty line
    
    * clean up
    
    * pass on requestId from nginx
    
    * remove node:cluster again
    
    * clean up
    
    * send heartbeat immediately
    
    * fix inconsistent naming
    
    * refactor using async functions
    
    * clean up
    
    * add CAR validation
    
    * fix debug
    
    * disable l2 integration in main network
    
    * refactor
    
    * refactor
    
    * enable l2 fire and forget
    
    * add car extraction
    
    * disable l2 fallback on file path req
    
    * use asyncHandler
    
    * remove cluster again (merge error)

--- container/shim/bin/shim.js
+++ container/shim/bin/shim.js
@@ -1,5 +1,4 @@
 import app from '../src/index.js'
-import { cpus } from 'node:os'
 import parseArgs from 'minimist'
 import http from 'node:http'
 
@@ -14,57 +13,43 @@ import {
 import { trapServer } from '../src/utils/trap.js'
 import { debug } from '../src/utils/logging.js'
 
-import cluster from 'node:cluster'
 import { submitRetrievals, initLogIngestor } from '../src/modules/log_ingestor.js'
 
 const argv = parseArgs(process.argv.slice(2))
 
-if (cluster.isPrimary) {
-  debug('Saturn L1 Node')
-  debug.extend('id')(nodeId)
-  debug.extend('version')(NODE_VERSION)
-  debug.extend('important')('===== IMPORTANT =====')
-  debug.extend('important')(`Earnings will be sent to Filecoin wallet address: ${FIL_WALLET_ADDRESS}`)
-  debug.extend('important')(NODE_OPERATOR_EMAIL ? `Payment notifications and important update will be sent to: ${NODE_OPERATOR_EMAIL}` : 'NO OPERATOR EMAIL SET, WE HIGHLY RECOMMEND SETTING ONE')
-  debug.extend('important')('===== IMPORTANT =====')
+debug('Saturn L1 Node')
+debug.extend('id')(nodeId)
+debug.extend('version')(NODE_VERSION)
+debug.extend('important')('===== IMPORTANT =====')
+debug.extend('important')(`Earnings will be sent to Filecoin wallet address: ${FIL_WALLET_ADDRESS}`)
+debug.extend('important')(NODE_OPERATOR_EMAIL ? `Payment notifications and important update will be sent to: ${NODE_OPERATOR_EMAIL}` : 'NO OPERATOR EMAIL SET, WE HIGHLY RECOMMEND SETTING ONE')
+debug.extend('important')('===== IMPORTANT =====')
 
-  for (let i = 0; i < cpus().length; i++) {
-    cluster.fork()
+process.on('SIGQUIT', shutdown)
+process.on('SIGINT', shutdown)
+
+setTimeout(async function () {
+  if (argv.register !== false) {
+    await register(true).catch(err => {
+      debug(`Failed to register ${err.name} ${err.message}`)
+      process.exit(1)
+    })
   }
 
-  cluster.on('exit', () => {
-    if (Object.keys(cluster.workers).length === 0) {
-      debug('All servers closed')
-      shutdownCluster()
-    }
-  })
+  // Start log ingestor
+  await initLogIngestor()
+}, 500)
 
-  process.on('SIGQUIT', shutdownCluster)
-  process.on('SIGINT', shutdownCluster)
+const server = http.createServer(app)
+server.listen(PORT, '127.0.0.1', async () => {
+  debug.extend('server')('shim process running')
+})
 
-  setTimeout(async function () {
-    if (argv.register !== false) {
-      await register(true).catch(err => {
-        debug(`Failed to register ${err.name} ${err.message}`)
-        process.exit(1)
-      })
-    }
+server.keepAliveTimeout = 60 * 60 * 1000
 
-    // Start log ingestor
-    await initLogIngestor()
-  }, 500)
-} else {
-  const server = http.createServer(app)
-  server.listen(PORT, '127.0.0.1', async () => {
-    debug.extend('server')('shim process running')
-  })
+trapServer(server)
 
-  server.keepAliveTimeout = 60 * 60 * 1000
-
-  trapServer(server)
-}
-
-async function shutdownCluster () {
+async function shutdown () {
   try {
     await Promise.allSettled([
       submitRetrievals(),
@@ -73,9 +58,7 @@ async function shutdownCluster () {
   } catch (err) {
     debug(`Failed during shutdown: ${err.name} ${err.message}`)
   } finally {
-    if (Object.keys(cluster.workers).length === 0) {
-      debug('Exiting...')
-      process.exit(0)
-    }
+    debug('Exiting...')
+    process.exit(0)
   }
 }
