1417d93b23abab6927f037357c8f3879893b7afc
Julian Gruber <julian@juliangruber.com>
2022-08-31 10:41:35 +0200

    add GET /ipfs/:cid -> respond from ipfs gateway -> simple response

--- container/shim/test/test.js
+++ container/shim/test/test.js
@@ -4,20 +4,16 @@ import app from '../src/index.js'
 import fetch, { Headers } from 'node-fetch'
 import http from 'node:http'
 import { promisify } from 'node:util'
-import { DEV_VERSION, TESTING_CID, nodeId } from '../src/config.js'
+import {
+  DEV_VERSION,
+  IPFS_GATEWAY_ORIGIN,
+  nodeId,
+  TESTING_CID
+} from '../src/config.js'
 import fsPromises from 'node:fs/promises'
 import { join, dirname } from 'node:path'
 import { fileURLToPath } from 'node:url'
 import nock from 'nock'
-
-nock.disableNetConnect()
-nock.enableNetConnect('localhost')
-
-async function createServer () {
-  const server = http.createServer(app)
-  await promisify(server.listen.bind(server))()
-  return { server, address: `http://localhost:${server.address().port}` }
-}
 
 const testCAR = await fsPromises.readFile(join(
   dirname(fileURLToPath(import.meta.url)),
@@ -25,6 +21,19 @@ const testCAR = await fsPromises.readFile(join(
   'public',
   `${TESTING_CID}.car`
 ))
+
+nock.disableNetConnect()
+nock.enableNetConnect('localhost')
+
+nock(IPFS_GATEWAY_ORIGIN)
+  .get('/ipfs/CID')
+  .reply(200, testCAR)
+
+async function createServer () {
+  const server = http.createServer(app)
+  await promisify(server.listen.bind(server))()
+  return { server, address: `http://localhost:${server.address().port}` }
+}
 
 test('L1 node', async t => {
   const { server, address } = await createServer()
@@ -65,7 +74,24 @@ test('L1 node', async t => {
       )
     })
     await t.todo('respond from L2')
-    await t.todo('respond from ipfs gateway')
+    await t.test('respond from ipfs gateway', async t => {
+      await t.test('simple response', async t => {
+        const res = await fetch(`${address}/ipfs/CID`)
+        assert.strictEqual(res.status, 200)
+        assert.deepStrictEqual(
+          Buffer.from(await (await res.blob()).arrayBuffer()),
+          testCAR
+        )
+      })
+      await t.todo('formats')
+      await t.todo('?filename')
+      await t.todo('?download')
+      await t.todo('timeout')
+      await t.todo('user-agent')
+      await t.todo('bad gateway response')
+      await t.todo('proxy response headers')
+      await t.todo('premature request end')
+    })
   })
   await t.todo('GET /ipfs/:cid/:path*')
   await t.todo('GET /register/:l2NodeId')
