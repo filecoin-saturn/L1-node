4f900866d6536ab8bfbffd7dc6fd7b027eaca2e9
Julian Gruber <julian@juliangruber.com>
2022-09-06 10:33:54 +0200

    add cid validation

--- container/shim/src/index.js
+++ container/shim/src/index.js
@@ -11,6 +11,7 @@ import pDefer from 'p-defer'
 import pTimeout from 'p-timeout'
 import timers from 'node:timers/promises'
 import asyncHandler from 'express-async-handler'
+import { CID } from 'multiformats'
 
 import { addRegisterCheckRoute } from './modules/registration.js'
 import {
@@ -75,6 +76,12 @@ app.get('/favicon.ico', (req, res) => {
 
 const handleCID = asyncHandler(async (req, res) => {
   const cid = req.params.cid
+  try {
+    CID.parse(cid)
+  } catch (err) {
+    return res.status(400).end('Invalid CID')
+  }
+
   const format = getResponseFormat(req)
 
   debug(`Cache miss for ${req.path}`)
