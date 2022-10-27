f87abd4a27365fd7a141bb5949ccbec573c3db31
Joao Andrade <joao.andrade@protocol.ai>
2022-10-27 17:14:27 +0100

    Improve quoting on start.sh

--- container/shim/start.sh
+++ container/shim/start.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# patch DNS to use the one the host passed to docker
-host_resolver="$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}' | tr '\n' ' ')"
+# patch DNS to use the ones the host passed to docker
+host_resolver="$(grep nameserver /etc/resolv.conf | awk '{print $2}' | tr '\n' ' ')"
 sed -i'' -e "s/resolver\s.*\s/resolver $host_resolver/" /etc/nginx/confs/tls_proxy.conf
 
-echo $(date -u) "[container] booting"
+echo "$(date -u) [container] booting"
 
-echo $(date -u) "[container] CPUs: $(nproc)"
-echo $(date -u) "[container] Memory: $(awk '(NR<4)' /proc/meminfo | tr -d '  ' | tr '\n' ' ')"
-echo $(date -u) "[container] Disk: $(df -h /usr/src/app/shared | awk '(NR>1)')"
-[ -f /sys/block/sda/queue/rotational ] && echo $(date -u) "[container] SDD:" $([ $(cat /sys/block/sda/queue/rotational) == "0" ] && echo 'true' || echo 'false')
-[ -f /sys/block/vda/queue/rotational ] && echo $(date -u) "[container] SDD:" $([ $(cat /sys/block/vda/queue/rotational) == "0" ] && echo 'true' || echo 'false')
+echo "$(date -u) [container] CPUs: $(nproc)"
+echo "$(date -u) [container] Memory: $(awk '(NR<4)' /proc/meminfo | tr -d '  ' | tr '\n' ' ')"
+echo "$(date -u) [container] Disk: $(df -h /usr/src/app/shared | awk '(NR>1)')"
+[ -f /sys/block/sda/queue/rotational ] && echo "$(date -u) [container] SDD: $([ "$(cat /sys/block/sda/queue/rotational)" == "0" ] && echo 'true' || echo 'false')"
+[ -f /sys/block/vda/queue/rotational ] && echo "$(date -u) [container] SDD: $([ "$(cat /sys/block/vda/queue/rotational)" == "0" ] && echo 'true' || echo 'false')"
 
 # Create if not exists
 mkdir -p /usr/src/app/shared/ssl
@@ -19,10 +19,10 @@ L1_CONF_FILE=/etc/nginx/conf.d/L1.conf
 
 # If we have a cert, start the shim and nginx, else just the shim
 if [ -f "/usr/src/app/shared/ssl/node.crt" ]; then
-  echo $(date -u) "[container] SSL config available, starting TLS nginx and node shim";
+  echo "$(date -u) [container] SSL config available, starting TLS nginx and node shim";
   cp /etc/nginx/confs/tls_proxy.conf $L1_CONF_FILE;
 else
-  echo $(date -u) "[container] SSL config unavailable, starting non-TLS nginx and node shim";
+  echo "$(date -u) [container] SSL config unavailable, starting non-TLS nginx and node shim";
   cp /etc/nginx/confs/non_tls_proxy.conf $L1_CONF_FILE;
 fi
 
