8474c9a624a7a8b5fbcccd0ba0b4e4cb49d0da44
Diego Rodriguez Baquero <diego@protocol.ai>
2022-11-01 11:21:20 +0000

    Show env variables in logs

--- update.sh
+++ update.sh
@@ -9,32 +9,33 @@ if pidof -o %PPID -x "update.sh" > /dev/null; then
   exit
 fi
 
-echo -n $(date -u) "Checking for update.sh script updates... "
-
 target=$SATURN_HOME/update.sh
+
+echo -n $(date -u) "Checking for auto-update script ($target) updates... "
+
 if wget -O "$target.tmp" -T 10 -t 3 "https://raw.githubusercontent.com/filecoin-saturn/L1-node/main/update.sh" && [[ -s "$target.tmp" ]] && [ $(stat -c %s "$target.tmp") -ne $(stat -c %s "$target") ]
 then
   mv -f "$target.tmp" "$target"
   chmod +x "$target"
-  echo "updated update.sh script successfully!"
+  echo "updated $target script successfully!"
   exit
 else
-  echo "update.sh script up to date"
+  echo "$target script up to date"
   rm -f "$target.tmp"
 fi
 
-echo -n $(date -u) "Checking for Saturn L1 node updates... "
+echo -n $(date -u) "Checking for Saturn $SATURN_NETWORK network L1 node updates... "
 
 out=$(sudo docker pull ghcr.io/filecoin-saturn/l1-node:$SATURN_NETWORK)
 
 if [[ $out != *"up to date"* ]]; then
-  echo $(date -u) "New Saturn L1 node version found!"
+  echo $(date -u) "New Saturn $SATURN_NETWORK network L1 node version found!"
 
   random_sleep=$[ ( $RANDOM % 3600 ) ]
   echo $(date -u) "Waiting for $random_sleep seconds..."
   sleep $random_sleep
 
-  echo -n $(date -u) "Draining L1 node... "
+  echo -n $(date -u) "Draining $SATURN_NETWORK network L1 node... "
   sudo docker kill --signal=SIGTERM saturn-node >> /dev/null
   sleep 600
   echo "restarting...."
@@ -54,5 +55,5 @@ if [[ $out != *"up to date"* ]]; then
 
   echo "Updated to latest version successfully!"
 else
-  echo "Saturn L1 node up to date"
+  echo "Saturn $SATURN_NETWORK network L1 node up to date"
 fi
