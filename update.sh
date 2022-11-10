6e27657e8273eb63a7f712e29c4b5837a8751e0d
Diego Rodriguez Baquero <diego@protocol.ai>
2022-11-10 16:58:29 -0500

    Update run script too

--- update.sh
+++ update.sh
@@ -9,19 +9,34 @@ if pidof -o %PPID -x "update.sh" > /dev/null; then
   exit
 fi
 
-target=$SATURN_HOME/update.sh
+update_target=$SATURN_HOME/update.sh
 
-echo -n "$(date -u) Checking for auto-update script ($target) updates... "
+echo -n "$(date -u) Checking for auto-update script ($update_target) updates... "
 
-if wget -O "$target.tmp" -T 10 -t 3 "https://raw.githubusercontent.com/filecoin-saturn/L1-node/main/update.sh" && [[ -s "$target.tmp" ]] && [ "$(stat -c %s "$target.tmp")" -ne "$(stat -c %s "$target")" ]
+if wget -O "$update_target.tmp" -T 10 -t 3 "https://raw.githubusercontent.com/filecoin-saturn/L1-node/main/update.sh" && [[ -s "$update_target.tmp" ]] && [ "$(stat -c %s "$update_target.tmp")" -ne "$(stat -c %s "$update_target")" ]
 then
-  mv -f "$target.tmp" "$target"
-  chmod +x "$target"
-  echo "updated $target script successfully!"
+  mv -f "$update_target.tmp" "$update_target"
+  chmod +x "$update_target"
+  echo "updated $update_target script successfully!"
   exit
 else
-  echo "$target script up to date"
-  rm -f "$target.tmp"
+  echo "$update_target script up to date"
+  rm -f "$update_target.tmp"
+fi
+
+run_target=$SATURN_HOME/run.sh
+
+echo -n "$(date -u) Checking for run script ($run_target) updates... "
+
+if wget -O "$run_target.tmp" -T 10 -t 3 "https://raw.githubusercontent.com/filecoin-saturn/L1-node/main/run.sh" && [[ -s "$run_target.tmp" ]] && [ "$(stat -c %s "$run_target.tmp")" -ne "$(stat -c %s "$run_target")" ]
+then
+  mv -f "$run_target.tmp" "$run_target"
+  chmod +x "$run_target"
+  echo "updated $run_target script successfully!"
+  exit
+else
+  echo "$run_target script up to date"
+  rm -f "$run_target.tmp"
 fi
 
 echo -n "$(date -u) Checking for Saturn $SATURN_NETWORK network L1 node updates... "
