bc6db22e3a179c024d14408187310c8853eb0e83
Joao Andrade <joao.andrade@protocol.ai>
2023-03-01 15:07:07 +0000

    Cleanup docker_compose_update.sh

--- docker_compose_update.sh
+++ docker_compose_update.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -eu
 
-curl -fs https://raw.githubusercontent.com/filecoin-saturn/L1-node/main/docker_compose_update.sh -o docker_compose_update.sh
+branch="update_docker_compose"
+repo="https://raw.githubusercontent.com/filecoin-saturn/L1-node/$branch"
 
-curl -fs https://raw.githubusercontent.com/filecoin-saturn/L1-node/main/docker-compose.yml -o docker-compose.yml.curl
+curl -fs "$repo/docker_compose_update.sh" -o docker_compose_update.sh
+
+curl -fs "$repo/docker-compose.yml" -o docker-compose.yml.curl
 if ! diff docker-compose.yml.curl docker-compose.yml >/dev/null; then
 				mv -f docker-compose.yml.curl docker-compose.yml
 				sudo docker compose up -d
