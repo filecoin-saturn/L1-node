afeae1410b999ab50c018ff581749a935e6d7e65
Diego Rodriguez Baquero <diego@protocol.ai>
2023-03-03 11:34:46 -0500

    Persist backup cert and key

--- container/shim/src/modules/tls.js
+++ container/shim/src/modules/tls.js
@@ -9,13 +9,15 @@ const debug = Debug.extend("tls");
 export const SSL_PATH = "/usr/src/app/shared/ssl";
 export const CERT_PATH = `${SSL_PATH}/node.crt`;
 export const KEY_PATH = `${SSL_PATH}/node.key`;
+export const BACKUP_CERT_PATH = `${SSL_PATH}/node.backup.crt`;
+export const BACKUP_KEY_PATH = `${SSL_PATH}/node.backup.key`;
 
 export const certExists = await fsPromises.stat(CERT_PATH).catch((_) => false);
 
 export async function getNewTLSCert(registerOptions) {
   const response = await fetch(`${ORCHESTRATOR_URL}/register`, registerOptions);
   const body = await response.json();
-  const { cert, key } = body;
+  const { cert, key, backupCert, backupKey } = body;
 
   if (!response.ok || !cert || !key) {
     debug("Received status %d with body: %o", response.status, body);
@@ -25,9 +27,20 @@ export async function getNewTLSCert(registerOptions) {
   debug("TLS certificate and key received, persisting to shared volume...");
 
   await saveCertAndKey(cert, key);
+  if (backupCert && backupKey) {
+    await saveBackupCertAndKey(backupCert, backupKey);
+  }
 }
 
 async function saveCertAndKey(cert, key) {
   debug("Saving cert and key");
   return await Promise.all([fsPromises.writeFile(CERT_PATH, cert), fsPromises.writeFile(KEY_PATH, key)]);
+}
+
+async function saveBackupCertAndKey(backupCert, backupKey) {
+  debug("Saving cert and key");
+  return await Promise.all([
+    fsPromises.writeFile(BACKUP_CERT_PATH, backupCert),
+    fsPromises.writeFile(BACKUP_KEY_PATH, backupKey),
+  ]);
 }
