26c3974bab538066955f0173a1ac7496412ba807
Joao Andrade <joao.andrade@protocol.ai>
2023-03-04 13:55:07 +0000

    Match paths for badbits

--- container/nginx/njs/badbits.js
+++ container/nginx/njs/badbits.js
@@ -3,18 +3,26 @@ const crypto = require("crypto");
 
 const badbits = {};
 const data = JSON.parse(fs.readFileSync("/etc/nginx/denylist.json"));
-
 data.forEach((entry) => (badbits[entry.anchor] = true));
 
-// implementation ref: https://github.com/protocol/bifrost-infra/blob/af46340bd830728b38a0ea632ca517d04277f78c/ansible/roles/nginx_conf_denylist/files/lua/helpers.lua#L80
-// TODO implement matching paths
+const ipfsRegex = /^\/ipfs\/(\w+)(\/?.*)/;
+
+// TODO implement matching CID paths
 // TODO convert CID v0 to CID v1
+// implementation ref: https://github.com/protocol/bifrost-infra/blob/af46340bd830728b38a0ea632ca517d04277f78c/ansible/roles/nginx_conf_denylist/files/lua/helpers.lua#L80
 function filterCID(req) {
 	const vars = req.variables;
-	// check if root hash(`CID/`) is blocked via denylist.json
-	const cid = vars.cid_path.split("/")[2] + "/";
+	const matches = vars.cid_path.match(ipfsRegex);
+	if (!matches) {
+		return req.return(200);
+	}
 
-	const hashedCID = crypto.createHash("sha256").update(cid).digest("hex");
+	const cid = matches[1];
+	// check if root hash(`CID/`) is blocked via denylist.json
+	const hashedCID = crypto
+		.createHash("sha256")
+		.update(cid + "/")
+		.digest("hex");
 	req.log(cid);
 	req.log(hashedCID);
 
