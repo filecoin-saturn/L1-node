65a665e6a50f097bfdb1b0f14b2bf5fea7a79dec
Joao Andrade <joao.andrade@protocol.ai>
2023-03-04 12:58:11 +0000

    Fix matching root cids

--- container/nginx/njs/badbits.js
+++ container/nginx/njs/badbits.js
@@ -6,9 +6,13 @@ const data = JSON.parse(fs.readFileSync("/etc/nginx/denylist.json"));
 
 data.forEach((entry) => (badbits[entry.anchor] = true));
 
+// implementation ref: https://github.com/protocol/bifrost-infra/blob/af46340bd830728b38a0ea632ca517d04277f78c/ansible/roles/nginx_conf_denylist/files/lua/helpers.lua#L80
+// TODO implement matching paths
+// TODO convert CID v0 to CID v1
 function filterCID(req) {
 	const vars = req.variables;
-	const cid = vars.cid_path.split("/")[2];
+	// check if root hash(`CID/`) is blocked via denylist.json
+	const cid = vars.cid_path.split("/")[2] + "/";
 
 	const hashedCID = crypto.createHash("sha256").update(cid).digest("hex");
 	req.log(cid);
