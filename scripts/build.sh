16474f35e4666854eab08f92f34190b52effe5d5
Joao Andrade <joao.andrade@protocol.ai>
2023-03-06 15:04:42 +0000

    Cleanup code

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-echo $(date -u) "[host] Building docker image"
+echo "$(date -u) [host] Building docker image"
 
 # Build the docker image and tag it
 docker build -t saturn-node --build-arg ORCHESTRATOR_URL=http://host.docker.internal:10365 .
 
-echo $(date -u) "[host] Build done"
+echo "$(date -u) [host] Build done"
