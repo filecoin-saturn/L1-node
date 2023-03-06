f1e62add816674379825336b94da3dac3e05b77e
Joao Andrade <joao.andrade@protocol.ai>
2023-03-06 14:48:45 +0000

    Use a script to test badbits

---
+++ scripts/test_badbits.sh
@@ -0,0 +1,18 @@
+set -eux
+
+port="$1"
+
+test_cid () {
+				cid="$1"
+				expected="$2"
+				code="$(curl -sw '%{http_code}\n' -o /dev/null "http://localhost:${port}/ipfs/${cid}")"
+				test "$code" -eq "$expected" || exit 1
+}
+
+not_blocked=501
+blocked=403
+
+# negative test case
+test_cid "Qmbfrc4cF2X4KXbHuqD593SLnR2xj6hULYTnrj65wKWaKm" "$not_blocked"
+# positive test case
+test_cid "bafybeibvcisellj6bfzbas3csvioltujjmif5jqpdw5ykvvwujtvt6up7u" "$blocked"
