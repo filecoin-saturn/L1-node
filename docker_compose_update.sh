b790552c4c1c99827934298f2e6b03e044d9ff1d
Joao Andrade <joao.andrade@protocol.ai>
2023-03-07 12:53:21 +0000

    Wait for container restarts when auto-updating docker compose

--- docker_compose_update.sh
+++ docker_compose_update.sh
@@ -11,5 +11,5 @@ curl -fs "$repo/docker_compose_update.sh" -o docker_compose_update.sh
 curl -fs "$repo/docker-compose.yml" -o docker-compose.yml.curl
 if ! diff docker-compose.yml.curl docker-compose.yml >/dev/null; then
     mv -f docker-compose.yml.curl docker-compose.yml
-    sudo docker compose up -d
+    sudo docker compose up -d -t 10800
 fi
