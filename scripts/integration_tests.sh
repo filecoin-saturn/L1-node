721470d5b99380619f7cab7d054a19dce9633522
tchardin <tdotchardin@gmail.com>
2023-04-13 08:54:17 -0600

    set byte range

--- scripts/integration_tests.sh
+++ scripts/integration_tests.sh
@@ -16,7 +16,7 @@ test_range_request () {
   code="$(curl -sw "%{http_code}\n" -o partial.car -H "Accept: application/vnd.ipld.car" "${base_url}/ipfs/${cid}")"
   test "$code" -eq 200 || exit 1
   ls -lh partial.car
-  car ls -v partial.car
+  ./car ls -v partial.car
 }
 
 # we're good this this response code, as going further means a Lassie fetch
@@ -34,4 +34,4 @@ test_cid "bafybeidgnebuxvarpnw2grmkgnamu6cv6" "$blocked"
 curl -LO -s https://github.com/ipld/go-car/releases/download/v2.8.0/go-car_2.8.0_linux_amd64.tar.gz && tar xzf go-car_2.8.0_linux_amd64.tar.gz
 
 # simple range request
-test_range_request "bafybeifpz6onienrgwvb3mw5rg7piq5jh63ystjn7s5wk6ttezy2gy5xwu/Mexico.JPG"
+test_range_request "bafybeifpz6onienrgwvb3mw5rg7piq5jh63ystjn7s5wk6ttezy2gy5xwu/Mexico.JPG?bytes=0:1048576"
