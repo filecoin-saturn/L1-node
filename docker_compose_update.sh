4e595d526cb66c7f07fdb29949957c583b703e17
Joao Andrade <joao.andrade@protocol.ai>
2023-05-12 15:46:30 +0100

    Have the auto-update script follow redirects

--- docker_compose_update.sh
+++ docker_compose_update.sh
@@ -17,9 +17,9 @@ fi
 
 repo="https://raw.githubusercontent.com/filecoin-saturn/L1-node/$ref"
 
-curl -fs "$repo/docker_compose_update.sh" -o docker_compose_update.sh
+curl -Lfs "$repo/docker_compose_update.sh" -o docker_compose_update.sh
 
-curl -fs "$repo/docker-compose.yml" -o docker-compose.yml.curl
+curl -Lfs "$repo/docker-compose.yml" -o docker-compose.yml.curl
 if ! diff docker-compose.yml.curl docker-compose.yml >/dev/null; then
     mv -f docker-compose.yml.curl docker-compose.yml
     sudo docker compose up -t 10800 -d
