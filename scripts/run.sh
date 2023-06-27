0979cf3bd71a605cc4d45bd9703105414e4a23d9
Eric Guan <guanzo91@gmail.com>
2023-06-26 19:12:56 -0700

    run container on host network

--- scripts/run.sh
+++ scripts/run.sh
@@ -16,14 +16,15 @@ echo "$(date -u) [host] Running Saturn node dev, with volume in $(pwd)/shared"
 
 # Start the docker image
 docker run --name saturn-node --rm $( [ "$CI" != "true" ] && printf "%s" "-it" ) \
+          --network=host \
           -v "$(pwd)/shared:/usr/src/app/shared:Z" \
           -e "FIL_WALLET_ADDRESS=$FIL_WALLET_ADDRESS" \
           -e "NODE_OPERATOR_EMAIL=$NODE_OPERATOR_EMAIL" \
           -e "IPFS_GATEWAY_ORIGIN=$IPFS_GATEWAY_ORIGIN" \
           -e "ORCHESTRATOR_REGISTRATION=$ORCHESTRATOR_REGISTRATION" \
           -e "SPEEDTEST_SERVER_CONFIG=$SPEEDTEST_SERVER_CONFIG" \
-          -e "LASSIE_ORIGIN=$LASSIE_ORIGIN" \
           -e "IS_CORE_L1=$IS_CORE_L1" \
+          -e "LASSIE_ORIGIN=$LASSIE_ORIGIN" \
           -e "LASSIE_ALLOW_PROVIDERS=$LASSIE_ALLOW_PROVIDERS" \
           -p "$HTTPS_PORT":443 -p "$HTTP_PORT":80 \
           saturn-node
