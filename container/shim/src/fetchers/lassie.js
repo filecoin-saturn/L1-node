5142c879aca808266469cd4720b150430cfdf105
Eric Guan <guanzo91@gmail.com>
2023-07-24 13:03:06 -0700

    use x-bfid over traceparent header

--- container/shim/src/fetchers/lassie.js
+++ container/shim/src/fetchers/lassie.js
@@ -159,7 +159,7 @@ export async function respondFromLassie(req, res, { cidObj, format }) {
       httpStatusCode: lassieRes?.status ?? null,
       requestErr,
       format,
-      traceparent: req.headers.traceparent,
+      traceparent: req.headers["x-bfid"],
       serverTiming: lassieRes?.headers.get("server-timing"),
     });
   }
