b4644d40005f49ceedfd470a556ec3cb3285a546
tchardin <tdotchardin@gmail.com>
2023-07-31 12:10:52 -0600

    test: stalling range on more content

--- scripts/integration_tests.sh
+++ scripts/integration_tests.sh
@@ -42,3 +42,5 @@ test_range_request "QmafUYju2Ab4ETi5HJG1cqjmnjs2xw9PUuBKzU7Hi3zvXU/MC_TheSource.
 
 # range request with offset
 test_range_request "bafybeifpz6onienrgwvb3mw5rg7piq5jh63ystjn7s5wk6ttezy2gy5xwu/Mexico.JPG?entity-bytes=1048576:2097152"
+
+test_range_request "bafybeieyd7lbyfjexjaqb6wvryaixjcmgnpljaca5umfwkbit72ozaotem?entity-bytes=1048576:2097151"
